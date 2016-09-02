20780dd4ddbf38eb9319f0465495a129756ce036
vilhelmfalkenmark <vilhelmfalkenmark@gmail.com>
2016-09-02 10:23:26 +0200

    working with the sliding functionality

--- dev/js/script.js
+++ dev/js/script.js
@@ -5,28 +5,30 @@ $( document ).ready(function() {
   var line = $("#line");
   var sliderContainer = $("#sliderContainer")
 
-
-
-
   dragger.bind('mousedown', function(e){
-
+    console.log(sliderContainer+ "klickad");
     var mouseX = e.pageX - line.offset().left; // MUSENS X VÄRDE I FÖRHÅLLANDE TILL LINJEN
     var innerOffset = mouseX - $(this).position().left;
 
-      dragger.bind('mousemove', function(e){
+      sliderContainer.bind('mousemove', function(e){
 
         console.log("hejsan!");
 
            mouseX = e.pageX - line.offset().left; // MUSENS X VÄRDE I FÖRHÅLLANDE TILL LINJEN
           // $(this).css("left",mouseX-innerOffset+"px")
           dragger.css("left",mouseX-innerOffset+"px")
-
-
-
       });
-
-
   });
+
+
+
+
+
+
+
+
+
+
   sliderContainer.bind('mouseup',function(){
     console.log("Nu är musen uppe");
       sliderContainer.unbind('mousemove');
