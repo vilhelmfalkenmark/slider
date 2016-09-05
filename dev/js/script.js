7248e7333bacfab5f1927feb37f4de0970c1a84f
vilhelmfalkenmark <vilhelmfalkenmark@gmail.com>
2016-09-05 17:25:23 +0200

    Fixed refactoring

--- dev/js/script.js
+++ dev/js/script.js
@@ -16,14 +16,44 @@ var donate = $('[data-id="donate"]');
 ///////////////////////////////////////////////
 var maxDonate = parseInt($('input[data-id="maxvalue"]').val());
 var priceRatio = maxDonate/100;
-
 var interval = parseInt($('input[data-id="interval"]').val());
 var item = $('input[data-id="item"]').val();
 var itemprice = parseInt($('input[data-id="itemprice"]').val());
 var itemAmount = $('[data-id="itemamount"]'); // HTML OUTPUT FOR HOW MANY ITEMS THE DONATED SUM CAN BUY
-
   itemamountHeader.html(minDonate);
   itemAmountItem.html(item);
+    var update = function (left, max, donateSum,unit) {
+      if(donateSum<0) {
+        donateSum = 0;
+      }
+      if(left >= max) { // MAX-VALUE ON THE LINE
+        itemamountHeader.html(maxDonate/itemprice)
+        sum.html(maxDonate)
+        donate.val(maxDonate)
+        dragger.css({
+             "right":"0",
+             "left": ""
+           })
+      }
+      else if(left <= 0) { // MIN-VALUE ON THE LINE
+        itemamountHeader.html(minDonate)
+        sum.html(minDonate)
+        donate.val(minDonate)
+        dragger.css({
+             "right":"",
+             "left": 0
+           })
+      }
+      else { // SOMEWHERE BETWEEN MAX AND MIN
+        itemamountHeader.html((donateSum/itemprice).toFixed(0));
+        sum.html(donateSum)
+        donate.val(donateSum)
+           dragger.css({
+             "right":"",
+             "left": left+unit
+           })
+      }
+    }
   ///////////////////////////////////////////////
   //////////// MOUSEDOWN
   ///////////////////////////////////////////////
@@ -41,11 +71,11 @@ var itemAmount = $('[data-id="itemamount"]'); // HTML OUTPUT FOR HOW MANY ITEMS 
       ///////////////////////////////////////////////
       sliderContainer.bind('mousemove', function(e){
            mouseX = e.pageX - line.offset().left;
-           let offsetLeft = ((mouseX-innerOffset)/line.width()*100);
+           var offsetLeft = ((mouseX-innerOffset)/line.width()*100);
            itemAmountItem.html(item)
            sumValue = offsetLeft+(draggerPercentage/2); // How far percentagewise the dragger is on the line
 
-          let donateSum = (sumValue*priceRatio).toFixed(0);
+          var donateSum = (sumValue*priceRatio).toFixed(0);
            update(offsetLeft,max,donateSum,"%")
       });
   });
@@ -60,90 +90,20 @@ var itemAmount = $('[data-id="itemamount"]'); // HTML OUTPUT FOR HOW MANY ITEMS 
   ///////////////////////////////////////////////
   if(screen.width <= 1024) {
     var screenWidth = screen.width;
-    var lineWidth = line.width();
-    var draggerWidth = dragger.width();
+     lineWidth = line.width();
+     draggerWidth = dragger.width();
     var diff = lineWidth - draggerWidth;
     var max = lineWidth - draggerWidth;
     var percentage;
     var lineLeft = line.offset().left;
 
-    // dragger.on('touchmove', dragger, function(e) {
-    //   e.preventDefault();
-    //     var xPos = e.originalEvent.touches[0].pageX;
-    //     let offsetLeft = xPos - 70; // CENTER OF GREEN CIRCLE
-    //     percentage = (($(this).offset().left - lineLeft)/diff)*100;
-    //     var donateSum = (percentage*priceRatio).toFixed(0);
-    //     update(offsetLeft,max,donateSum,"px")
-    //   });
-
     dragger.on('touchmove', dragger, function(e) {
-  e.preventDefault();
-    var xPos = e.originalEvent.touches[0].pageX;
-    var left = xPos - 70; // CENTER OF GREEN CIRCLE
-    percentage = (($(this).offset().left - lineLeft)/diff)*100;
-    var outputValue = (percentage*priceRatio).toFixed(0);
-    if(outputValue<0) {
-      outputValue = 0;
-    }
-
-
-    if(left >= max) {
-      dragger.css("left",max+"px")
-      itemamountHeader.html(maxDonate/itemprice)
-      sum.html(maxDonate)
-      donate.val(maxDonate)
-
-    }
-    else if(left <= 0) {
-      itemamountHeader.html(minDonate)
-      sum.html(minDonate)
-      donate.val(minDonate)
-      dragger.css("left","0px")
-
-    }
-    else {
-      itemamountHeader.html((outputValue/itemprice).toFixed(0));
-      sum.html(outputValue)
-      donate.val(outputValue)
-      dragger.css("left",left+"px")
-    }
-  });
-
-  }
-  function update(left, max, donateSum,unit) {
-
-    // alert("oinwefoinw")
-
-
-    if(donateSum<0) {
-      donateSum = 0;
-    }
-    if(left >= max) { // MAX-VALUE ON THE LINE
-      itemamountHeader.html(maxDonate/itemprice)
-      sum.html(maxDonate)
-      donate.val(maxDonate)
-      dragger.css({
-           "right":"0",
-           "left": ""
-         })
-    }
-    else if(left <= 0) { // MIN-VALUE ON THE LINE
-      itemamountHeader.html(minDonate)
-      sum.html(minDonate)
-      donate.val(minDonate)
-      dragger.css({
-           "right":"",
-           "left": 0
-         })
-    }
-    else { // SOMEWHERE BETWEEN MAX AND MIN
-      itemamountHeader.html((donateSum/itemprice).toFixed(0));
-      sum.html(donateSum)
-      donate.val(donateSum)
-         dragger.css({
-           "right":"",
-           "left": left+unit
-         })
-    }
+      e.preventDefault();
+        var xPos = e.originalEvent.touches[0].pageX;
+        var offsetLeft = xPos - 70; // CENTER OF GREEN CIRCLE
+        percentage = (($(this).offset().left - lineLeft)/diff)*100;
+        var donateSum = (percentage*priceRatio).toFixed(0);
+        update(offsetLeft,max,donateSum,"px")
+      });
   }
 });
